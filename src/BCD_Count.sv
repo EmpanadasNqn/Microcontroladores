7885b16e21f673823fa8bb21178be1f824ea1241
EmpanadasArg (Santino Ponchiardi) <santino.ponchiardi@mi.unc.edu.ar>
2026-08-31 09:15:04 -0300

    {M} sintax always_ff

--- src/BCD_Count.sv
+++ src/BCD_Count.sv
@@ -11,11 +11,12 @@ module BCD_Count(
 
     assign Q_carry = (Q_count >= 4'b1001) && (en && D_carry);
     
-    always_ff @(posedge clk)
+    always_ff @(posedge clk) begin
         if (rst) Q_count <= 4'b0000;
         else if (en && D_carry) begin
             if (Q_count >= 4'b1001) Q_count <= 4'b0000;
             else Q_count <= Q_count + 4'b0001;
         end
+    end
 
 endmodule
